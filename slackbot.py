c0ce32a10e27a388705c869dcbaf2e14eaef4976
gaunah <christoph@zelinski.de>
2019-04-19 17:23:51 +0200

    improved logging

--- slackbot.py
+++ slackbot.py
@@ -1,9 +1,10 @@
-from os import path
-from time import sleep
 import json
 import logging
+from os import path
+from time import sleep
 
 from slackclient import SlackClient
+
 
 
 class SlackBot:
@@ -16,14 +17,18 @@ class SlackBot:
             client:SlackClinet: client instance with the given api token to perfom api calls
             userIdDict:dict: contains {"id": "real_name"} of the users in a Slack Workspace
         """
+
         if not path.exists(token_file):
-            logging.critical(token_file + " not found!")
+            logger.critical(token_file + " not found!")
             exit(1)
 
         with open(token_file, 'rb') as f:
             api_token = (f.read().decode("latin-1")).strip()
+            if not api_token.startswith("xoxb-"):
+                logger.critical("malformed api token: " + api_token)
+                exit(1)
 
-        logging.info("token: " + api_token)
+        logger.debug("token: " + api_token)
         self.client = SlackClient(api_token)
         self.userIdDict = {}
 
@@ -31,7 +36,7 @@ class SlackBot:
         """
         This method posts a message to a public channel, private channel, or direct message/IM channel.
         """
-        logging.debug('send \"{}\" to \"{}\"'.format(msg, channel))
+        logger.debug('send \"{}\" to \"{}\"'.format(msg, channel))
         return self.client.api_call(
             "chat.postMessage",
             channel=channel,
@@ -53,14 +58,14 @@ class SlackBot:
         Starts the real time messaging loop
         """
         if self.client.rtm_connect(with_team_state=False):
-            logging.info("Connected to rtm api...")
+            logger.info("Connected to rtm api...")
             online = True
             while online:
                 event = self.client.rtm_read()
                 self._parse_rtm_event(event)
                 sleep(1)
         else:
-            logging.error("Connection Failed")
+            logger.error("Connection Failed")
 
     def _parse_rtm_event(self, event):
         """
@@ -70,20 +75,27 @@ class SlackBot:
         try:
             if len(event) > 0:
                 rsp = event[0]
-                if rsp["type"] == "message":
-                    # got a message
+                if rsp["type"] == "message":  # got a message
                     msg = rsp["text"]
                     userId = rsp["user"]
-                    print("msg: \"{}\" from \"{}\"".format(msg, self.userIdDict[userId]))
+                    print("msg: \"{}\" from \"{}\"".format(
+                        msg, self.userIdDict[userId]))
+                elif rsp["type"] == "hello":  # server hello
+                    logger.info("got hello from server")
+                elif rsp["type"] == "user_typing":  # user typing
+                    logger.info("{} is typing".format(
+                        self.userIdDict[rsp["user"]]))
+                elif rsp["type"] == "desktop_notification":  # notification
+                    logger.info("desktop_notification")
                 else:
-                    logging.warning(json.dumps(event, indent=2))
+                    logger.warning(json.dumps(event, indent=2))
         except KeyError as ke:
-            logging.error("KeyError: " + str(ke))
-            logging.error(json.dumps(event, indent=2))
-
+            logger.error("KeyError: " + str(ke))
+            logger.error(json.dumps(event, indent=2))
 
 
 def main():
+    logger.info("SlackBot started")
     sb = SlackBot("slack_api_token")
     sb.initUserIdDict()
     sb.enter_rtm_loop()
@@ -91,6 +103,11 @@ def main():
 
 if __name__ == "__main__":
     try:
+        logging.basicConfig(
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            level=logging.INFO,
+            filename="slackbot.log")
+        logger = logging.getLogger("SlackBot")
         main()
     except KeyboardInterrupt:
-        print("stopped by user.")
+        logger.info("stopped by user.")
